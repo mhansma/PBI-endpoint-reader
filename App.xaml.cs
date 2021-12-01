a8bb0e7995e29aed9f33e6d8360463e2c4f2e5df
Marco Hansma <Marco.Hansma@inspark.nl>
2021-12-01 20:50:46 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+namespace Power_BI_endpoint_reader
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mw = new();
+            if (e.Args.Length > 0)
+            {
+                mw.Server.Text = e.Args[0];
+            }
+
+            if (e.Args.Length > 1)
+            {
+                mw.Database.Text = e.Args[1];
+            }
+            mw.Show();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Power_BI_endpoint_reader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string ServerName;
+        public string DatabaseName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
